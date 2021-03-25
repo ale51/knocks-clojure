44870b72b56bce161353a2c80847b18bf446ea72
ale51 <naoki.m0513@gmail.com>
2021-03-25 19:30:01 +0900

    add knocks

--- src/knocks_clojure/core.clj
+++ src/knocks_clojure/core.clj
@@ -1,6 +1,50 @@
 (ns knocks-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;map
+(map #(+ % 1) [1 2 3])
+
+;filter
+(filter #(= 1 %) [1 2 3])
+
+;every
+(every? even? '(2 4 6))
+(every? even? '(2 4 5))
+
+;long
+(type (long 1))
+
+;int
+(type (int 1))
+
+;literal
+(type 1)
+(type 1.1)
+(type "a")
+
+;even?
+(even? 2)
+
+;assoc
+(assoc {} :age 32)
+
+;get-in
+(as-> {:persons [{:age 32 :name "naoki"} {:age 31 :name "yurina"}]} person
+      (get-in person [:persons 0 :name])
+      )
+
+;get-in(default value)
+(as-> {:persons [{:age 32 :name "naoki"} {:age 31 :name "yurina"}]} person
+      (get-in person [:persons 2 :name] "machida")
+      )
+
+;group by
+(def collection
+  [
+   {:type "A" :point 1}
+   {:type "A" :point 2}
+   {:type "A" :point 2}
+   {:type "B" :point 4}
+   ]
+  )
+(group-by :type collection)
+(group-by (juxt :type :point) collection)
