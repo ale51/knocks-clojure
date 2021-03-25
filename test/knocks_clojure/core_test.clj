3121429a384a2a6b19c5f602f1c317b4fe97cf59
ale51 <naoki.m0513@gmail.com>
2021-03-25 18:44:24 +0900

    initial commit

---
+++ test/knocks_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns knocks-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [knocks-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
